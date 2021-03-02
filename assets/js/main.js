5919984689ac29e1e96832d881623ef1eae3f3b0
gabriele <sozziga@gmail.com>
2021-03-02 20:20:56 +0100

    Completato

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,33 @@
 // Creo Array
 
-var email = [ email1, email2, email3 ]
+var email = [ "pippo@gmail.com", "pluto@libero.it", "pippo@libero.it", "pluto@gmail.com", "gabriele@gmail.com" ]
 
 console.log(email);
 
 // Chiedo la mail all'utente
 
-var mail = ("Inserisci un indirizzo mail");
+var mail = prompt ("Inserisci un indirizzo mail");
+
+console.log(mail);
 
 // Inserisco sentinella
 
-var sentinella = false;
+var breakLoop = false;
 
 // Creo FOR Array
 
 for (var i = 0; i < email.length; i++) {
   if (email[i] == mail) {
-    sentinella = true;
+    breakLoop = true;
   }
 }
 
 // Inserisco condizioni per msg
 
-if (sentinella) {
+if (breakLoop) {
   console.log("Email accepted");
+  document.getElementById('message').innerHTML = "Email accepted";
 } else {
   console.log("Email not found");
+  document.getElementById('message').innerHTML = "Email not found";
 }
